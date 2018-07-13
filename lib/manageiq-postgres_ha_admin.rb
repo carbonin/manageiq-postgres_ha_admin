b31444671ba9b581e00a110f936bbfc00cd6c7cd
Nick Carboni <ncarboni@redhat.com>
2018-07-13 15:52:58 -0400

    Rename FailoverDatabases to ServerStore
    This better describes that this class is what manages the local file
    to indicate the servers which we can use during failover.

--- lib/manageiq-postgres_ha_admin.rb
+++ lib/manageiq-postgres_ha_admin.rb
@@ -4,7 +4,7 @@ require 'manageiq/postgres_ha_admin/logging'
 require 'manageiq/postgres_ha_admin/null_logger'
 
 require 'manageiq/postgres_ha_admin/database_yml'
-require 'manageiq/postgres_ha_admin/failover_databases'
+require 'manageiq/postgres_ha_admin/server_store'
 require 'manageiq/postgres_ha_admin/failover_monitor'
 
 module ManageIQ
